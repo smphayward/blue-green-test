ce19d95cd9cf336386c01c399dcd84819378f95a
Shaun <smphayward@gmail.com>
2022-11-03 16:04:32 -0400

    Updated info.php

--- www-server-a/info.php
+++ www-server-a/info.php
@@ -4,9 +4,10 @@ header('Content-Type: application/json');
 if (!isset($myObj)) 
     $myObj = new stdClass();
 
-$myObj->removeAddress = $_SERVER['REMOTE_ADDR'];
-$myObj->requesterAddress = $_SERVER['HTTP_INK_REQUESTER_IP'];
-$myObj->requestHost = $_SERVER['HTTP_INK_REQUEST_HOST'];
+$myObj->remoteAddress = $_SERVER['REMOTE_ADDR'];
+$myObj->inkRequesterIpAddress = $_SERVER['HTTP_INK_REQUESTER_IP'];
+$myObj->inkRequestHost = $_SERVER['HTTP_INK_REQUEST_HOST'];
+$myObj->inkRequestId = $_SERVER['HTTP_INK_REQUEST_ID'];
 $myObj->hostname = gethostname();
 
 $myJSON = json_encode($myObj);
