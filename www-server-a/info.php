b8d48da98ced05351a25af4a38d0114bed3174bf
Shaun <smphayward@gmail.com>
2022-11-03 15:27:04 -0400

    Added info.php

---
+++ www-server-a/info.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json');
+$myObj->removeAddress = $_SERVER['REMOTE_ADDR'];
+$myObj->requesterAddress = $_SERVER['HTTP_INK_REQUESTER_IP'];
+$myObj->requestHost = $_SERVER['HTTP_INK_REQUEST_HOST'];
+$myObj->hostname = gethostname()
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
