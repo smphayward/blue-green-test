b4625430bd40beeb1708d96d3c73a9a37be87107
Shaun <smphayward@gmail.com>
2022-11-04 11:34:56 -0400

    Trying to make JSON pretty

--- www-server-b/east-west.php
+++ www-server-b/east-west.php
@@ -3,8 +3,10 @@
   echo "I am: " . gethostname() . "<br>";
 
   $jsonContents = file_get_contents("http://nginx/info.php");
+  $jsonDecoded = json_decode($jsonContents);
+  $jsonPretty = json_encode($jsonDecoded, JSON_PRETTY_PRINT);
 
   echo "Received from east/west call:<br>";
-  echo $jsonContents;
+  echo $jsonPretty;
 
 ?>
