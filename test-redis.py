a7ddebc8f0e899af47157f4115b55a71bc3fae26
Shaun <smphayward@gmail.com>
2023-01-18 11:06:07 -0500

    Added another sentinel to A. Created test-redis python script.

---
+++ test-redis.py
@@ -0,0 +1,20 @@
+from redis.sentinel import Sentinel
+
+# with IP based connections, a list of known node IP addresses is constructed
+# to allow connection even if any one of the nodes in the list is unavailable.
+sentinel_list = [
+('192.168.2.90', 26379),
+('192.168.2.90', 26380),
+('192.168.2.90', 26381)
+]
+
+# change this to the db name you want to connect
+db_name = 'db1'
+
+sentinel = Sentinel(sentinel_list, socket_timeout=0.1)
+r = sentinel.master_for(db_name, socket_timeout=0.1)
+
+# set key "foo" to value "bar"
+print r.set('foo', 'bar')
+# set value for key "foo"
+print r.get('foo')
